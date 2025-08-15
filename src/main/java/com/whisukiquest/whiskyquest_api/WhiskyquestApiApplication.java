3a99144440cc4f816ac5fdf19d1b5960c087c041
小久井　杏奈 <a591.k.0530@gmail.com>
2025-08-15 15:34:23 +0900

    first commit

---
+++ src/main/java/com/whisukiquest/whiskyquest_api/WhiskyquestApiApplication.java
@@ -0,0 +1,13 @@
+package com.whisukiquest.whiskyquest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyquestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyquestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whisukiquest/whiskyquest_api/WhiskyquestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whisukiquest.whiskyquest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyquestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
