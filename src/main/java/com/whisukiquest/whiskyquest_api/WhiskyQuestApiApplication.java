82d40884505b1279f9dc7a70889c191fc93ca8d6
小久井　杏奈 <a591.k.0530@gmail.com>
2025-09-01 23:28:30 +0900

    openAPI実装

--- src/main/java/com/whisukiquest/whiskyquest_api/WhiskyQuestApiApplication.java
+++ src/main/java/com/whisukiquest/whiskyquest_api/WhiskyQuestApiApplication.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "ウイスキー管理システム",
-		description = "ウイスキーの記録、評価を管理するシステムです。"  ))
+@OpenAPIDefinition(info = @Info(
+		title = "ウイスキー管理システム",
+		description = "ウイスキーの記録、評価を管理するシステムです。"
+))
 @SpringBootApplication
 public class WhiskyQuestApiApplication {
 
--- src/main/java/com/whisukiquest/whiskyquest_api/controller/WhiskyController.java
+++ src/main/java/com/whisukiquest/whiskyquest_api/controller/WhiskyController.java
@@ -6,6 +6,7 @@ import com.whisukiquest.whiskyquest_api.domain.WhiskyDetail;
 import com.whisukiquest.whiskyquest_api.domain.WhiskyInfo;
 import com.whisukiquest.whiskyquest_api.service.WhiskyService;
 import com.whisukiquest.whiskyquest_api.validation.Update;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * ウイスキーの記録、評価を管理するシステムです。"
+ * ウイスキーの検索や登録、更新などをREST APIとして実行されるcontrollerです。
 **/
 
 @Validated
@@ -35,47 +36,78 @@ public class WhiskyController {
     this.service = service;
   }
 
-  /**ウイスキー情報
-   *
-   * **/
-  //ユーザー詳細、登録したウイスキー詳細、評価詳細が取れてくる
+
+  /****
+   * ユーザー情報検索です。ユーザーIDに紐づくウイスキー情報と評価情報の一覧を取得します。
+   * @param userId ユーザーID
+   * @return ユーザー詳細、登録しているウイスキー情報と評価の一覧
+   */
+  @Operation(summary = "ユーザー詳細の検索",
+      description = "ユーザーIDをもとに、ユーザーが登録したウイスキー一覧、評価一覧の検索が行えます。")
   @GetMapping("/user/{userId}")
   public UserDetail getUserList(
       @PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int userId) {
     return service.searchUserDetail(userId);
   }
 
+  /****
+   * ウイスキー情報検索です。ウイスキーIDに紐づく評価一覧を取得します。
+   * @param whiskyId ウイスキーID
+   * @return ウイスキー詳細、評価詳細一覧
+   */
 
-  //ウイスキー詳細と、そのウイスキーに対しての評価一覧が取れてくる
+@Operation(summary = "ウイスキー詳細と評価一覧検索",
+description = "ウイスキーIDをもとに、ウイスキーの詳細、評価一覧の検索が行えます。")
   @GetMapping("/whisky/{whiskyId}")
   public WhiskyDetail getWhiskyList(
       @PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int whiskyId) {
     return service.searchWhiskyDetail(whiskyId);
   }
 
-  //ユーザー情報新規登録
+  /****
+   * ユーザーの新規登録が行えます。
+   * @param users ユーザー詳細
+   * @return 実行結果
+   */
+
+  @Operation(summary = "ユーザー新規登録",
+  description = "ユーザー新規登録が行えます。")
   @PostMapping("/registerUser")
   public ResponseEntity<Users> registerUser(@RequestBody Users users) {
     Users responseUser = service.registerUsers(users);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
   }
 
-  //ウイスキー情報と評価情報の新規登録
+  /****
+   * ウイスキー情報と評価情報の新規登録が行えます。
+   * @param whiskyInfo ウイスキー詳細
+   * @return 実行結果
+   */
+
+  @Operation(summary = "ウイスキー詳細と、そのウイスキーに対しての評価の登録",
+  description = "ウイスキー詳細と評価の登録が行えます。")
   @PostMapping("/registerWhisky")
   public ResponseEntity<WhiskyInfo> registerWhiskyInfo(@RequestBody @Valid WhiskyInfo whiskyInfo) {
     WhiskyInfo responseWhiskyInfo = service.registerWhiskyInfo(whiskyInfo);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseWhiskyInfo);
   }
 
-  //ユーザー情報の更新
-@PutMapping("/updateUser/{userId}")
+  /****
+   *ユーザーIDに紐づく詳細の更新が行えます。
+   * @param userId ユーザーID
+   * @param users ユーザー詳細
+   * @return 実行結果
+   */
+  @Operation(summary = "ユーザー情報の更新",
+description = "ユーザーIDをもとに、ユーザー情報の更新が行えます。")
+  @PutMapping("/updateUser/{userId}")
   public ResponseEntity<Users> updateUser(@PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int userId ,
     @Validated(Update.class) @RequestBody  Users users){
     Users responseUser = service.updateUser(users);
     return ResponseEntity.ok(responseUser);
 }
-
-//ウイスキー情報の更新
+@Operation(summary = "ウイスキー情報の更新",
+    description = "ウイスキーIDと評価IDをもとに、ウイスキー情報と評価情報の更新が行えます。")
   @PutMapping("/updateWhiskyInfo/{WhiskyId}/{RatingId}")
   public ResponseEntity<WhiskyInfo> updateWhisky(
       @PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int WhiskyId,
@@ -85,21 +117,24 @@ public class WhiskyController {
     return ResponseEntity.ok(responseWhiskyInfo);
   }
 
-  //ユーザー情報の論理削除
+  @Operation(summary = "ユーザー情報の論理削除",
+  description = "ユーザーIDをもとに、ユーザー情報の論理削除が行えます。")
   @PutMapping("/deleteUser/{userId}")
   public ResponseEntity<Void> deleteUser(@PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int userId) {
     service.deleteUser(userId);
     return ResponseEntity.ok().build();
   }
 
-  //ウイスキー情報の論理削除
+  @Operation(summary = "ウイスキー情報の論理削除",
+      description = "ユーザーIDをもとに、ユーザーが登録したウイスキー情報の論理削除が行えます。")
   @PutMapping("/deleteWhisky/{userId}")
   public ResponseEntity<Void> deleteWhisky(@PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int userId) {
     service.deleteWhisky(userId);
     return ResponseEntity.ok().build();
   }
 
-  //評価情報の論理削除
+  @Operation(summary = "評価情報の論理削除",
+      description = "ユーザーIDをもとに、ユーザーが登録した評価情報の論理削除が行えます。")
   @PutMapping("/deleteRating/{userId}")
   public ResponseEntity<Void> deleteRating(@PathVariable @NotBlank @Pattern(regexp = "^\\d+$") int userId) {
     service.deleteRating(userId);
