5f6db77766145013bca9ada9198975ecf897f3bb
小久井　杏奈 <a591.k.0530@gmail.com>
2025-09-15 00:32:14 +0900

    アノテーション追加

--- src/main/java/com/whisukiquest/whiskyquest_api/controller/WhiskyController.java
+++ src/main/java/com/whisukiquest/whiskyquest_api/controller/WhiskyController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * ウイスキーの検索や登録、更新などをREST APIとして実行されるcontrollerです。
 **/
-
+@CrossOrigin(origins = "http://localhost:3000")//異なるドメインからのリクエストを許可するアノテーション
 @Validated
 @RestController
 public class WhiskyController {
